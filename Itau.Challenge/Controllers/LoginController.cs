76f6be5c699062446398ecbc0c06ca90bdf44f83
willianwatanabe <yuuuh.watanabe@gmail.com>
2023-01-09 19:59:19 -0300

    [FEAT]: Tela de login

---
+++ Itau.Challenge/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using Itau.Challenge.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Itau.Challenge.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new UserModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserModel login)
+        {
+            //if (login.IsValid(login))
+            //{
+            //    //_notyf.Error("Preencha todos os campos corretamente!");
+            //    return View(login);
+            //}
+
+            //var usuarioParaLogin = await _autorizacaoService.VerificarSeUsuarioEstaAutorizado(login.Chave, login.Senha);
+            //if (!usuarioParaLogin.Valido)
+            //{
+            //    _notyf.Error(usuarioParaLogin.Mensagem);
+            //    return View(login);
+            //}
+
+            //await AutenticacaoHelper.AutenticarNoCookie(login.Chave, usuarioParaLogin, false, HttpContext);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
